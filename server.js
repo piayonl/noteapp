125c6138b3d23506a05cb268b558c4985c9b7c00
piayonl <piayonl@yahoo.com>
2020-11-04 08:48:01 -0500

    Procfile

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 3090;
+const port = 5050;
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
