f07e80ffda184d05c4ca750da320d75430bc34ec
piayonl <piayonl@yahoo.com>
2020-11-04 07:36:13 -0500

    get route

--- server.js
+++ server.js
@@ -1,73 +1,62 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
 const express = require("express");
-// Tells node that we are creating an "express" server
-const app = express ();
-const fs = require ("fs");
-const path = require ("path");
-// const DirectoryPath = path.resolve(__dirname, "db");
-// const outputPath = path.join(DirectoryPath, "db.json");
-// Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 4060
+const path = require("path");
+const fs = require("fs");
 
+const app = express();
+const port = 3090;
+const mainDir = path.join(__dirname, "/public");
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-// app.use(express.static("public"));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
-module.exports = function(app) {
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
 app.get("/api/notes", function(req, res) {
- fs.readFile("db/db.json", "utf8",(err, data)=>{
-     if (err) throw err;
-      console.log(data);
-     return res.json(data);
- });
-});
- app.post("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", "utf8",(err, data)=>{
-        if (err) throw err;
-        const dbJson = JSON.parse(data);
-        let newID = 0
-        if(dbJson !== [] || dbJson !==[{}]){
-            newID = dbJson[dbJson.length-1].id + 1;
-        }
-
-        const newEntry = {id: newID, ...req.body};
-        dbJson.push(newEntry);
-        fs.writeFile("db/db.json",JSON.stringify(dbJson, null, 2, + ","), (err)=> {
-            if (err) throw err;
-            return res.json(newEntry);
-        })
-    });
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-};
-
-//  app.delete("/api/notes/:id", async (req, res) => {
-//     fs.readFile(outputPath, "utf8",(err, data)=>{
-//         if (err) throw err;
-//         const newID = JSON.findByIdAndDelete(req.params.id);
-//              fs.writeFile(outputPath,JSON.stringify(newID), null, + ","), (err)=> {
-//                  if (err) throw err;
-//                  return res.json(newEntry);
-//            };
-//         });
-//     }); 
-
-// =============================================================================
-// LISTENER
-// "starts" our server
-// =============================================================================
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
 });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.listen(port, function() {
+    console.log(`Now listening to port ${port}. Enjoy your stay!`);
+})
